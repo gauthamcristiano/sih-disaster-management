21473b981312388c007b79216b994ac23e9464b6
gauthamcristiano <gauthamgautham546@gmail.com>
2026-09-15 14:50:01 +0530

    Remove environment variable for API_URL

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -62,8 +62,7 @@ type Inputs = {
   vegetationLoss: number;
 };
 
-const API_URL =
-  import.meta.env.VITE_API_URL || "http://127.0.0.1:8000";
+const API_URL = "http://127.0.0.1:8000";
 
 const locations: Location[] = [
   {
