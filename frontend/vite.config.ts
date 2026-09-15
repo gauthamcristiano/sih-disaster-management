9c8ef64f654895904bf18e2ecad0e2d4ea064531
gauthamcristiano <gauthamgautham546@gmail.com>
2026-09-15 13:52:26 +0530

    Set base path for Vite configuration

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,7 +2,9 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  base: "/sih-disaster-management/",
   plugins: [react()],
+
   server: {
     port: 5173,
     host: "localhost",
